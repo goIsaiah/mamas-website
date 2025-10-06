9e2e3e44de469262fe6bc8b8ca6d1b3b7d297d6f
goIsaiah <goisaiah@outlook.com>
2025-10-06 10:10:57 -0400

    Fixed clicking on images in projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,6 +20,58 @@ export default function Home() {
   return (
     <section id="projects" className="py-16">
       <h2 className="text-4x1 font-bold text-center">Projects</h2>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{ hidden: {}, visible: { transition: { staggerChildren: 0.2 } }, }}
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 px-4 mt-8"
+      >
+        {projects.map((project,idx) => (
+          <motion.div
+            key={idx}
+            variants={{ hidden: { opacity: 0, y: 50 }, visible: { opacity: 1, y: 0 }, }} transition={{ duration: 0.6 }} whileHover={{ y: -10, scale: 1.03 }}
+          >
+            <Card className="w-full flex flex-col items-center text-center">
+              <CardHeader>
+                <CardTitle className="text-xl font-bold">
+                  {project.title}
+                </CardTitle>
+                <div className="mt-4">
+                  <img src={project.image} alt={`${project.title} Screenshot`} className="w-full h-auto rounded-lg shadow-lg cursor-pointer" onClick={() => setSelectedImage(project.image)} />
+                </div>
+              </CardHeader>
+              <CardContent className="px-6 pb-6">
+                <p className="text-gray-700">{project.description}</p>
+                <div className="mt-4 flex flex-wrap justify-center gap-4">
+                  {project.links.map((link, linkIdx) => ( <Link key={linkIdx} href={link.href}> <Button className="mt-2">{link.label}</Button> </Link> ))}
+                </div>
+              </CardContent>
+            </Card>
+          </motion.div>
+        ))}
+      </motion.div>
+
+      <AnimatePresence>
+        {selectedImage && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0}}
+            className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50"
+            onClick={() => setSelectedImage(null)}
+          >
+            <motion.div
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              className="bg-white rounded-lg shadow-lg max-w-4xl w-full p-4"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <img src={selectedImage} alt="Expanded" className="w-full h-auto rounded-lg" />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
-  )
+  );
 }
