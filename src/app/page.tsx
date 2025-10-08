c6ade7fb4d26bcd43d6e8d5d82115e98c4ead07f
goIsaiah <goisaiah@outlook.com>
2025-10-08 10:29:05 -0400

    Fixed alignment issues in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,21 +26,28 @@ import { ChevronDown, User, MonitorCheck, BookOpenText } from "lucide-react";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-      <h1 className="text-5xl font-bold invert-text">Welcome!</h1>
+    <div className="relative flex flex-col items-center min-h-screen overflow-hidden pt-24">
+      <h1 className="text-5xl font-bold mb-40 z-10 text-white text-center">Welcome!</h1>
 
-      <div className="z-10 grid grid-cols-1 sm:grid-cols-2 gap-8 mb-20">
-        <Link href="/bakery" className="hover:scale-105 transition-transform duration-300">
-          <Card className="w-80 h-80 overflow-hidden shadow-lg">
-            <img src="/images/penguin-baker.png" alt="Sample 1" className="w-full h-full object-cover" />
-          </Card>
-        </Link>
+      <div className="z-10 flex flex-wrap justify-center gap-24 mb-32">
+        <div className="flex flex-col items-center">
+          <Link href="/bakery" className="hover:scale-105 transition-transform duration-300">
+            <Card className="w-96 h-96 overflow-hidden bg-transparent border-0 shadow-[0_0_40px_rgba(255,192,203,0.5)]">
+              <img src="/images/penguin-baker.png" alt="Sample 1" className="w-full h-full object-cover" />
+            </Card>
+          </Link>
+          <p className="text-5xl font-bold text-white text-center mt-8">Mama's Bakery</p>
+        </div>
 
-        <Link href="/crafts" className="hover:scale-105 transition-transform duration-300">
-          <Card className="w-80 h-80 overflow-hidden shadow-lg">
-            <img src="/images/penguin-crafts.png" alt="Sample 2" className="w-full h-full object-cover" />
-          </Card>
-        </Link>
+        <div className="flex flex-col items-center">
+          <Link href="/crafts" className="hover:scale-105 transition-transform duration-300">
+            <Card className="w-96 h-96 overflow-hidden bg-transparent border-0 shadow-[0_0_40px_rgba(255,192,203,0.5)]">
+              <img src="/images/penguin-crafts.png" alt="Sample 2" className="w-full h-full object-cover" />
+            </Card>
+          </Link>
+          <p className="text-5xl font-bold text-white text-center mt-8">Mama's Crafts</p>
+        </div>
+
       </div>
       <div className="wave"></div>
       <div className="wave"></div>
