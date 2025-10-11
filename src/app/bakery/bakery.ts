0536774e36be55faf9b0112f6ea99451a58821f7
goIsaiah <goisaiah@outlook.com>
2025-10-11 10:34:50 -0400

    Added more cakes

--- src/app/bakery/bakery.ts
+++ src/app/bakery/bakery.ts
@@ -20,4 +20,49 @@ export const bakery: Bakery[] = [
         description: "Pineapple coconut upside down cake with the oval Corelle casserole dish",
         image: "/images/bakery/pineapple upside down.jpg",
     },
+    {
+        title: "7\" Chocolate Pistachio Cake",
+        description: "7\" Double Layer Chocolate Pistachio Cake",
+        image: "/images/bakery/choco pistachio 1.jpg",
+    },
+    {
+        title: "Carrot Cake",
+        description: "7\" carrot cake baked with coconut, walnuts and raisins; topped with cream cheese frosting, shredded coconut, pecans and mini marzipan carrots.  Made for Sase's birthday.",
+        image: "/images/bakery/carrot cake.jpg",
+    },
+    {
+        title: "Carrot Loaf Cakes",
+        description: "8\"x4\" carrot loaf cakes, with 1.5x batch cream cheese frosting",
+        image: "/images/bakery/carrot loaf cakes.jpg",
+    },
+    {
+        title: "Mini Cheesecakes",
+        description: "5 muffin sized mini cheesecakes",
+        image: "/images/bakery/mini cheesecakes.jpg",
+    },
+    {
+        title: "Cream Cheese Banana Cake",
+        description: "8\" Banana cake with cream cheese frosting",
+        image: "/images/bakery/banana cake.jpg",
+    },
+    {
+        title: "Crumble Topping Banana Cake",
+        description: "8\" round banana cake with crumble topping (2\" tall)",
+        image: "/images/bakery/crumble banana cake.jpg",
+    },
+    {
+        title: "Peach Custard Cake",
+        description: "Peach custard cake 🍑 with whipped cream frosting",
+        image: "/images/bakery/peach custard cake.jpg",
+    },
+    {
+        title: "Mocha Cake",
+        description: "Mocha cake",
+        image: "/images/bakery/mocha cake.jpg",
+    },
+    {
+        title: "Blueberry Yogurt Cake",
+        description: "8\" (1.5\" tall) blueberry yogurt cake, with blueberry happy face :)",
+        image: "/images/bakery/blueberry yogurt cake.jpg",
+    },
 ];
