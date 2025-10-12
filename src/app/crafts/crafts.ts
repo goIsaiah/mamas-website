89e527e2754dc0aa97299273f9438e7b2d7018bb
drevNG <133802806+drevNG@users.noreply.github.com>
2025-10-12 12:02:30 -0400

    Update crafts.ts
    Added 9 crafts items (from Teal Crochet Bag to Bouquet of Roses Coaster)

--- src/app/crafts/crafts.ts
+++ src/app/crafts/crafts.ts
@@ -6,8 +6,48 @@ export type Crafts = {
 
 export const crafts: Crafts[] = [
     {
-        title: "Double Layer Strawberry Vanilla Cake",
-        description: "6\" Double Layer Strawberry Vanilla Cake",
-        image: "/images/bakery/double layer strawberry 1.jpg",
+        title: "Teal Crochet Bag",
+        description: "Teal crochet bag",
+        image: "images/crafts/Teal Crochet Bag.jpg",
+    },
+    {
+        title: "Crochet Toques",
+        description: "Red and gray crochet toques",
+        image: "images/crafts/Crochet Toques.jpg",
+    },
+    {
+        title: "Crochet Shoes",
+        description: "Light blue crochet shoes with white laces",
+        image: "images/crafts/Crochet Shoes.jpg",
+    },
+    {
+        title: "Crochet Teapot Cozy",
+        description: "Blue crochet teapot cozy",
+        image: "images/crafts/Crochet Teapot Cozy.jpg",
+    },
+    {
+        title: "Gray Crochet Bag",
+        description: "Gray crochet bag",
+        image: "images/crafts/Gray Crochet Bag.jpg",
+    },
+    {
+        title: "Crochet Waterbottle Holder",
+        description: "Pink crochet waterbottle holder",
+        image: "images/crafts/Crochet Waterbottle Holder.jpg",
+    },
+    {
+        title: "White Crochet Pouch",
+        description: "White crochet pouch",
+        image: "images/crafts/White Crochet Pouch.jpg",
+    },
+    {
+        title: "Roses Coaster",
+        description: "Roses coaster",
+        image: "images/crafts/Roses Coaster.jpg",
+    },
+    {
+        title: "Bouquet of Roses Coaster",
+        description: "Bouquet of roses coaster",
+        image: "images/crafts/Bouquet of roses coaster.jpg",
     },
 ];
